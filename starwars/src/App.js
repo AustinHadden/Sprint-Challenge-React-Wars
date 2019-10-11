741f77d848ccb973777e29340428caa279b313f8
AustinHadden <vitovalletori@gmail.com>
2019-10-11 11:34:04 -0500

    finished making and styling my character cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Characters from "./components/Characters";
 import './App.css';
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+      <Characters />
+    </div>    
   );
 }
 
